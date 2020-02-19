9088684d6d92acea2ab301a1f493b09287a929e5
Jason Bosco <mail@jasonbos.co>
2020-02-18 18:32:11 -0800

    Updates for Instantsearch v4

--- src/ResponseAdapter.js
+++ src/ResponseAdapter.js
@@ -12,31 +12,63 @@ export class ResponseAdapter {
         ...typesenseHit.document
       };
       adaptedHit.objectID = typesenseHit.document.id;
-
-      // Algolia lists all searchable attributes in this key, even if there are no matches
-      // So do the same and then override highlights
-      adaptedHit._highlightResult = Object.assign(
-        {},
-        ...Object.keys(typesenseHit.document).map(attribute => ({
-          [attribute]: {
-            value: typesenseHit.document[attribute],
-            matchLevel: "none",
-            matchedWords: []
-          }
-        }))
-      );
-      typesenseHit.highlights.forEach(highlight => {
-        adaptedHit._highlightResult[highlight.field] = {
-          value: highlight.snippet,
-          matchLevel: "full",
-          matchedWords: [] // Todo: Fix MatchedWords
-        };
-      });
+      adaptedHit._highlightResult = this._adaptHighlightResult(typesenseHit);
       // Todo: Fix Snippets and Highlights
       adaptedHit._snippetResult = adaptedHit._highlightResult;
       return adaptedHit;
     });
     return adaptedResult;
+  }
+
+  _adaptHighlightTag(value) {
+    return value
+      .replace(new RegExp("<mark>", "g"), "__ais-highlight__")
+      .replace(new RegExp("</mark>", "g"), "__/ais-highlight__");
+  }
+
+  _adaptHighlightResult(typesenseHit) {
+    // Algolia lists all searchable attributes in this key, even if there are no matches
+    // So do the same and then override highlights
+
+    const result = Object.assign(
+      {},
+      ...Object.keys(typesenseHit.document).map(attribute => ({
+        [attribute]: {
+          value: typesenseHit.document[attribute],
+          matchLevel: "none",
+          matchedWords: []
+        }
+      }))
+    );
+
+    typesenseHit.highlights.forEach(highlight => {
+      result[highlight.field] = {
+        value: highlight.snippet,
+        matchLevel: "full",
+        matchedWords: [] // Todo: Fix MatchedWords
+      };
+    });
+
+    // Now convert any values that have an array value
+    // Also, replace highlight tag
+    Object.keys(result).forEach(attribute => {
+      const { value, matchLevel, matchedWords } = result[attribute];
+      if (Array.isArray(value)) {
+        result[attribute] = [];
+        value.forEach(v => {
+          result[attribute].push({
+            value: this._adaptHighlightTag(`${v}`),
+            matchLevel: matchLevel, // TODO: Fix MatchLevel for array
+            matchedWords: matchedWords // TODO: Fix MatchedWords for array
+          });
+        });
+      } else {
+        // Convert all values to strings
+        result[attribute].value = this._adaptHighlightTag(`${value}`);
+      }
+    });
+
+    return result;
   }
 
   _adaptFacets(typesenseFacetCounts) {
