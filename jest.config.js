74b9507d2c6d9c7a32d1435051cfe6046927c5e1
Jason Bosco <mail@jasonbos.co>
2020-02-28 12:39:31 -0800

    Setup test summary in CircleCI

--- jest.config.js
+++ jest.config.js
@@ -97,7 +97,7 @@ module.exports = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: ["default", "jest-junit"],
 
   // Automatically reset mock state between every test
   // resetMocks: false,
