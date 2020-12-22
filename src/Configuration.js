e6f0397a1497ee980f14e13f8fb6db13c81c0ff5
Ashwin Mohan Ram <ashwinmram@gmail.com>
2020-12-22 15:01:15 +0800

    Removing unnecessary code

--- src/Configuration.js
+++ src/Configuration.js
@@ -1,7 +1,20 @@
 "use strict";
 
-export class Configuration {
-  constructor(options = {}) {
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Configuration = void 0;
+
+var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
+
+var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
+
+var Configuration = /*#__PURE__*/function () {
+  function Configuration() {
+    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    (0, _classCallCheck2["default"])(this, Configuration);
     this.server = options.server || {
       masterNode: {
         host: "localhost",
@@ -10,31 +23,21 @@ export class Configuration {
         protocol: "http"
       }
     };
-
     this.additionalSearchParameters = options.additionalSearchParameters || {};
-
-    this.additionalSearchParameters.queryBy =
-      this.additionalSearchParameters.queryBy || "";
-
-    this.additionalSearchParameters.sortBy =
-      this.additionalSearchParameters.sortBy || "";
-
-    this.additionalSearchParameters.groupBy =
-      this.additionalSearchParameters.groupBy || "";
-
-    this.additionalSearchParameters.perPage =
-      this.additionalSearchParameters.perPage || "10";
-
-    this.additionalSearchParameters.highlightFullFields =
-      this.additionalSearchParameters.highlightFullFields ||
-      this.additionalSearchParameters.queryBy;
+    this.additionalSearchParameters.queryBy = this.additionalSearchParameters.queryBy || "";
+    this.additionalSearchParameters.sortBy = this.additionalSearchParameters.sortBy || "";
+    this.additionalSearchParameters.highlightFullFields = this.additionalSearchParameters.highlightFullFields || this.additionalSearchParameters.queryBy;
   }
 
-  validate() {
-    if (this.additionalSearchParameters.queryBy.length === 0) {
-      throw new Error(
-        "Missing required parameter: additionalSearchParameters.queryBy"
-      );
+  (0, _createClass2["default"])(Configuration, [{
+    key: "validate",
+    value: function validate() {
+      if (this.additionalSearchParameters.queryBy.length === 0) {
+        throw new Error("Missing required parameter: additionalSearchParameters.queryBy");
+      }
     }
-  }
-}
+  }]);
+  return Configuration;
+}();
+
+exports.Configuration = Configuration;
