2d67a6da7f34b878ffc0b04f63492490162f6e5a
Ashwin Mohan Ram <ashwinmram@gmail.com>
2020-12-22 15:02:28 +0800

    Removing unnecessary code

--- src/Configuration.js
+++ src/Configuration.js
@@ -1,20 +1,7 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.Configuration = void 0;
-
-var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
-
-var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
-
-var Configuration = /*#__PURE__*/function () {
-  function Configuration() {
-    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-    (0, _classCallCheck2["default"])(this, Configuration);
+export class Configuration {
+  constructor(options = {}) {
     this.server = options.server || {
       masterNode: {
         host: "localhost",
@@ -23,21 +10,25 @@ var Configuration = /*#__PURE__*/function () {
         protocol: "http"
       }
     };
+
     this.additionalSearchParameters = options.additionalSearchParameters || {};
-    this.additionalSearchParameters.queryBy = this.additionalSearchParameters.queryBy || "";
-    this.additionalSearchParameters.sortBy = this.additionalSearchParameters.sortBy || "";
-    this.additionalSearchParameters.highlightFullFields = this.additionalSearchParameters.highlightFullFields || this.additionalSearchParameters.queryBy;
+
+    this.additionalSearchParameters.queryBy =
+      this.additionalSearchParameters.queryBy || "";
+
+    this.additionalSearchParameters.sortBy =
+      this.additionalSearchParameters.sortBy || "";
+
+    this.additionalSearchParameters.highlightFullFields =
+      this.additionalSearchParameters.highlightFullFields ||
+      this.additionalSearchParameters.queryBy;
   }
 
-  (0, _createClass2["default"])(Configuration, [{
-    key: "validate",
-    value: function validate() {
-      if (this.additionalSearchParameters.queryBy.length === 0) {
-        throw new Error("Missing required parameter: additionalSearchParameters.queryBy");
-      }
+  validate() {
+    if (this.additionalSearchParameters.queryBy.length === 0) {
+      throw new Error(
+        "Missing required parameter: additionalSearchParameters.queryBy"
+      );
     }
-  }]);
-  return Configuration;
-}();
-
-exports.Configuration = Configuration;
+  }
+}
--- src/SearchRequestAdapter.js
+++ src/SearchRequestAdapter.js
@@ -1,223 +1,181 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+export class SearchRequestAdapter {
+  static get INDEX_NAME_MATCHING_REGEX() {
+    return new RegExp("^(.+?)(?=(/sort/(.*))|$)");
+  }
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.SearchRequestAdapter = void 0;
-
-var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
-
-var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
-
-var _slicedToArray2 = _interopRequireDefault(require("@babel/runtime/helpers/slicedToArray"));
-
-var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
-
-var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
-
-var SearchRequestAdapter = /*#__PURE__*/function () {
-  (0, _createClass2["default"])(SearchRequestAdapter, null, [{
-    key: "INDEX_NAME_MATCHING_REGEX",
-    get: function get() {
-      return new RegExp("^(.+?)(?=(/sort/(.*))|$)");
-    }
-  }]);
-
-  function SearchRequestAdapter(instantsearchRequest, typesenseClient, additionalSearchParameters) {
-    (0, _classCallCheck2["default"])(this, SearchRequestAdapter);
+  constructor(
+    instantsearchRequest,
+    typesenseClient,
+    additionalSearchParameters
+  ) {
     this.instantsearchRequest = instantsearchRequest;
     this.typesenseClient = typesenseClient;
     this.additionalSearchParameters = additionalSearchParameters;
   }
 
-  (0, _createClass2["default"])(SearchRequestAdapter, [{
-    key: "_adaptBaseFilters",
-    value: function _adaptBaseFilters(filters) {
-      var adaptedResult = "";
+  _adaptBaseFilters(filters) {
+    let adaptedResult = "";
 
-      if (!filters) {
-        return adaptedResult;
-      }
-
-      var temp = [];
-      var intermediateFacetFilters = {};
-      var facetFilters = filters.split("AND");
-      facetFilters.forEach(function (part) {
-        if (part.includes("OR")) {
-          var subparts = part.split("OR");
-          temp.push(subparts);
-        } else {
-          temp.push(part);
-        }
-      });
-      temp.flat().forEach(function (facetFilter) {
-        var _facetFilter$split = facetFilter.split(":"),
-            _facetFilter$split2 = (0, _slicedToArray2["default"])(_facetFilter$split, 2),
-            facetName = _facetFilter$split2[0],
-            facetValue = _facetFilter$split2[1];
-
-        intermediateFacetFilters[facetName] = intermediateFacetFilters[facetName] || [];
-        intermediateFacetFilters[facetName].push(facetValue);
-      });
-      adaptedResult = Object.entries(intermediateFacetFilters).map(function (_ref) {
-        var _ref2 = (0, _slicedToArray2["default"])(_ref, 2),
-            facet = _ref2[0],
-            values = _ref2[1];
-
-        return "".concat(facet, ":=[").concat(values.join(","), "]");
-      }).join(" && ");
+    if (!filters) {
       return adaptedResult;
     }
-  }, {
-    key: "_adaptFacetFilters",
-    value: function _adaptFacetFilters(facetFilters) {
-      var adaptedResult = "";
 
-      if (!facetFilters) {
-        return adaptedResult;
+    let temp = [];
+    let intermediateFacetFilters = {};
+
+    let facetFilters = filters.split("AND")
+
+    facetFilters.forEach(part => {
+      if(part.includes("OR")) {
+        let subparts = part.split("OR")
+        temp.push(subparts)
+      } else {
+        temp.push(part)
       }
+    })
 
-      var intermediateFacetFilters = {}; // Need to transform:
-      // faceFilters = [["facet1:value1", "facet1:value2"], "facet2:value3"]]
-      //
-      // Into this:
-      // intermediateFacetFilters = {
-      //     "facet1": ["value1", "value2"],
-      //     "facet2": ["value1", "value2"]
-      // }
+    temp.flat().forEach(facetFilter => {
+      let [facetName, facetValue] = facetFilter.split(":");
 
-      facetFilters.flat().forEach(function (facetFilter) {
-        var _facetFilter$split3 = facetFilter.split(":"),
-            _facetFilter$split4 = (0, _slicedToArray2["default"])(_facetFilter$split3, 2),
-            facetName = _facetFilter$split4[0],
-            facetValue = _facetFilter$split4[1];
+      intermediateFacetFilters[facetName] =
+        intermediateFacetFilters[facetName] || [];
+      intermediateFacetFilters[facetName].push(facetValue);
+    });
 
-        intermediateFacetFilters[facetName] = intermediateFacetFilters[facetName] || [];
-        intermediateFacetFilters[facetName].push(facetValue);
-      }); // Need to transform this:
-      // intermediateFacetFilters = {
-      //     "facet1": ["value1", "value2"],
-      //     "facet2": ["value1"]
-      // }
-      //
-      // Into this:
-      // facet1:=[value1,value2] && facet2:=value1
 
-      adaptedResult = Object.entries(intermediateFacetFilters).map(function (_ref3) {
-        var _ref4 = (0, _slicedToArray2["default"])(_ref3, 2),
-            facet = _ref4[0],
-            values = _ref4[1];
+    adaptedResult = Object.entries(intermediateFacetFilters)
+      .map(([facet, values]) => `${facet}:=[${values.join(",")}]`)
+      .join(" && ");
 
-        return "".concat(facet, ":=[").concat(values.join(","), "]");
-      }).join(" && ");
+    return adaptedResult
+  }
+
+  _adaptFacetFilters(facetFilters) {
+    let adaptedResult = "";
+
+    if (!facetFilters) {
       return adaptedResult;
     }
-  }, {
-    key: "_adaptNumericFilters",
-    value: function _adaptNumericFilters(numericFilters) {
-      var adaptedResult = "";
 
-      if (!numericFilters) {
-        return adaptedResult;
-      }
+    const intermediateFacetFilters = {};
 
-      adaptedResult = numericFilters.map(function (numericFilter) {
-        return numericFilter.replace(new RegExp("(>|<=)"), ":$1");
-      }).join(" && ");
+    // Need to transform:
+    // faceFilters = [["facet1:value1", "facet1:value2"], "facet2:value3"]]
+    //
+    // Into this:
+    // intermediateFacetFilters = {
+    //     "facet1": ["value1", "value2"],
+    //     "facet2": ["value1", "value2"]
+    // }
+
+    facetFilters.flat().forEach(facetFilter => {
+      const [facetName, facetValue] = facetFilter.split(":");
+      intermediateFacetFilters[facetName] =
+        intermediateFacetFilters[facetName] || [];
+      intermediateFacetFilters[facetName].push(facetValue);
+    });
+
+    // Need to transform this:
+    // intermediateFacetFilters = {
+    //     "facet1": ["value1", "value2"],
+    //     "facet2": ["value1"]
+    // }
+    //
+    // Into this:
+    // facet1:=[value1,value2] && facet2:=value1
+
+    adaptedResult = Object.entries(intermediateFacetFilters)
+      .map(([facet, values]) => `${facet}:=[${values.join(",")}]`)
+      .join(" && ");
+
+    return adaptedResult;
+  }
+
+  _adaptNumericFilters(numericFilters) {
+    let adaptedResult = "";
+
+    if (!numericFilters) {
       return adaptedResult;
     }
-  }, {
-    key: "_adaptFilters",
-    value: function _adaptFilters(filters, facetFilters, numericFilters) {
-      var adaptedFilters = [];
-      adaptedFilters.push(this._adaptBaseFilters(filters));
-      adaptedFilters.push(this._adaptFacetFilters(facetFilters));
-      adaptedFilters.push(this._adaptNumericFilters(numericFilters));
-      return adaptedFilters.filter(function (filter) {
-        return filter !== "";
-      }).join(" && ");
+
+    adaptedResult = numericFilters
+      .map(numericFilter => numericFilter.replace(new RegExp("(>|<=)"), ":$1"))
+      .join(" && ");
+
+    return adaptedResult;
+  }
+
+  _adaptFilters(filters, facetFilters, numericFilters) {
+    const adaptedFilters = [];
+
+    adaptedFilters.push(this._adaptBaseFilters(filters));
+    adaptedFilters.push(this._adaptFacetFilters(facetFilters));
+    adaptedFilters.push(this._adaptNumericFilters(numericFilters));
+
+    return adaptedFilters.filter(filter => filter !== "").join(" && ");
+  }
+
+  _adaptIndexName(indexName) {
+    return indexName.match(this.constructor.INDEX_NAME_MATCHING_REGEX)[1];
+  }
+
+  _adaptSortBy(indexName) {
+    return indexName.match(this.constructor.INDEX_NAME_MATCHING_REGEX)[3];
+  }
+
+  _buildSearchParameters() {
+    const params = this.instantsearchRequest.params;
+    const indexName = this.instantsearchRequest.indexName;
+
+    const snakeCasedAdditionalSearchParameters = {};
+    for (const [key, value] of Object.entries(
+      this.additionalSearchParameters
+    )) {
+      snakeCasedAdditionalSearchParameters[this._camelToSnakeCase(key)] = value;
     }
-  }, {
-    key: "_adaptIndexName",
-    value: function _adaptIndexName(indexName) {
-      return indexName.match(this.constructor.INDEX_NAME_MATCHING_REGEX)[1];
+
+    const typesenseSearchParams = Object.assign(
+      {},
+      snakeCasedAdditionalSearchParameters
+    );
+
+    Object.assign(typesenseSearchParams, {
+      q: params.query === "" ? "*" : params.query,
+      facet_by: [params.facets].flat().join(","),
+      filter_by: this._adaptFilters(params.filters, params.facetFilters, params.numericFilters),
+      sort_by: this._adaptSortBy(indexName) ? this._adaptSortBy(indexName) : this.additionalSearchParameters.sortBy,
+      max_facet_values: params.maxValuesPerFacet,
+      page: (params.page || 0) + 1
+    });
+
+    if (params.hitsPerPage) {
+      typesenseSearchParams.per_page = params.hitsPerPage;
     }
-  }, {
-    key: "_adaptSortBy",
-    value: function _adaptSortBy(indexName) {
-      return indexName.match(this.constructor.INDEX_NAME_MATCHING_REGEX)[3];
+
+    if (params.facetQuery) {
+      typesenseSearchParams.facet_query = `${params.facetName}:${params.facetQuery}`;
+      typesenseSearchParams.per_page = 0;
     }
-  }, {
-    key: "_buildSearchParameters",
-    value: function _buildSearchParameters() {
-      var params = this.instantsearchRequest.params;
-      var indexName = this.instantsearchRequest.indexName;
-      var snakeCasedAdditionalSearchParameters = {};
 
-      for (var _i = 0, _Object$entries = Object.entries(this.additionalSearchParameters); _i < _Object$entries.length; _i++) {
-        var _Object$entries$_i = (0, _slicedToArray2["default"])(_Object$entries[_i], 2),
-            key = _Object$entries$_i[0],
-            value = _Object$entries$_i[1];
+    // console.log(params);
+    // console.log(typesenseSearchParams);
 
-        snakeCasedAdditionalSearchParameters[this._camelToSnakeCase(key)] = value;
-      }
+    return typesenseSearchParams;
+  }
 
-      var typesenseSearchParams = Object.assign({}, snakeCasedAdditionalSearchParameters);
-      Object.assign(typesenseSearchParams, {
-        q: params.query === "" ? "*" : params.query,
-        facet_by: [params.facets].flat().join(","),
-        filter_by: this._adaptFilters(params.filters, params.facetFilters, params.numericFilters),
-        sort_by: this._adaptSortBy(indexName) ? this._adaptSortBy(indexName) : this.additionalSearchParameters.sortBy,
-        max_facet_values: params.maxValuesPerFacet,
-        page: (params.page || 0) + 1
-      });
+  _camelToSnakeCase(str) {
+    return str
+      .split(/(?=[A-Z])/)
+      .join("_")
+      .toLowerCase();
+  }
 
-      if (params.hitsPerPage) {
-        typesenseSearchParams.per_page = params.hitsPerPage;
-      }
-
-      if (params.facetQuery) {
-        typesenseSearchParams.facet_query = "".concat(params.facetName, ":").concat(params.facetQuery);
-        typesenseSearchParams.per_page = 0;
-      } // console.log(params);
-      // console.log(typesenseSearchParams);
-
-
-      return typesenseSearchParams;
-    }
-  }, {
-    key: "_camelToSnakeCase",
-    value: function _camelToSnakeCase(str) {
-      return str.split(/(?=[A-Z])/).join("_").toLowerCase();
-    }
-  }, {
-    key: "request",
-    value: function () {
-      var _request = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee() {
-        return _regenerator["default"].wrap(function _callee$(_context) {
-          while (1) {
-            switch (_context.prev = _context.next) {
-              case 0:
-                return _context.abrupt("return", this.typesenseClient.collections(this._adaptIndexName(this.instantsearchRequest.indexName)).documents().search(this._buildSearchParameters()));
-
-              case 1:
-              case "end":
-                return _context.stop();
-            }
-          }
-        }, _callee, this);
-      }));
-
-      function request() {
-        return _request.apply(this, arguments);
-      }
-
-      return request;
-    }()
-  }]);
-  return SearchRequestAdapter;
-}();
-
-exports.SearchRequestAdapter = SearchRequestAdapter;
+  async request() {
+    return this.typesenseClient
+      .collections(this._adaptIndexName(this.instantsearchRequest.indexName))
+      .documents()
+      .search(this._buildSearchParameters());
+  }
+}
