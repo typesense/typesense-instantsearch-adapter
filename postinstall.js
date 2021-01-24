a299dae6d56a0cdfda36503aedf49bf0cff4fc7c
Jason Bosco <mail@jasonbos.co>
2021-01-24 15:16:27 -0800

    Add postinstall message

---
+++ postinstall.js
@@ -0,0 +1,8 @@
+const packageJson = require("./package.json");
+
+if (packageJson.version.includes("0.4.")) {
+  console.warn(
+    "\x1b[33m%s\x1b[0m",
+    "Note: v0.4.x of typesense-instantsearch-adapter only works with Typesense Server v0.19.x and above."
+  );
+}
