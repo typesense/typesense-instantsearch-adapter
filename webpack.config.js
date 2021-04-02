73ac3388929bc8beb8c3f50b69800447947d7450
Jason Bosco <mail@jasonbos.co>
2021-04-02 13:01:14 -0700

    Generate dist bundle

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/TypesenseInstantsearchAdapter"),
+  mode: "production",
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "typesense-instantsearch-adapter.min.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  }
+};
