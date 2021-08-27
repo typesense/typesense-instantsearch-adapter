3a5078485909b7d6a2658a52529d7e69467667eb
Jason Bosco <mail@jasonbos.co>
2021-08-27 14:35:48 -0700

    Swap file contents

--- test/federatedSearch.test.js
+++ test/federatedSearch.test.js
@@ -1,17 +1,27 @@
-describe("Geo Search - List View", () => {
+describe("Federated Search", () => {
   beforeAll(require("./support/beforeAll"), 60 * 1000);
 
   beforeEach(async () => {
-    return page.goto("http://localhost:3000/geosearch_list_view.html");
+    return page.goto("http://localhost:3000/federated_search.html");
   }, 30 * 1000);
 
-  describe("aroundLatLng via configure widget", () => {
-    it("renders results within a within the specified radius around lat/lng", async () => {
-      await expect(page).toMatchElement("#stats", {
-        text: "56 results found",
+  describe("when searching for a term", () => {
+    beforeEach(async () => {
+      return expect(page).toFill("#searchbox input[type=search]", "Charger");
+    });
+
+    it("renders the results across multiple indices", async () => {
+      await expect(page).toMatchElement("#product-stats", {
+        text: "294 results found",
       });
-      return await expect(page).toMatchElement("#hits .ais-Hits-item:nth-of-type(1)", {
-        text: "LAX",
+      await expect(page).toMatchElement("#product-hits .ais-Hits-item:nth-of-type(1) .hit-name", {
+        text: "Charger",
+      });
+      await expect(page).toMatchElement("#brand-stats", {
+        text: "2 results found",
+      });
+      return await expect(page).toMatchElement("#brand-hits .ais-Hits-item:nth-of-type(1) .hit-name", {
+        text: "ChargeHub",
       });
     });
   });
