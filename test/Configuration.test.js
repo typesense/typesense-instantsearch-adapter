c1c1162f6c1883fcead74628a8b2b34bfa759899
Jason Bosco <mail@jasonbos.co>
2023-01-16 12:43:53 -0600

    Allow presets to be used

--- test/Configuration.test.js
+++ test/Configuration.test.js
@@ -3,7 +3,7 @@ import { Configuration } from "../src/Configuration";
 describe("Configuration", () => {
   describe(".validate", () => {
     describe("using query_by", () => {
-      it("throws an error if query_by is not set anywhere", () => {
+      it("throws an error if query_by or preset is not set anywhere", () => {
         const subject = new Configuration({});
 
         expect(() => {
@@ -43,6 +43,63 @@ describe("Configuration", () => {
             },
             collection2: {
               query_by: "name",
+            },
+          },
+        });
+
+        expect(() => {
+          subject.validate();
+        }).not.toThrow();
+      });
+      it("does not throw an error if preset is set in additionalSearchParameters", () => {
+        const subject = new Configuration({
+          additionalSearchParameters: {
+            preset: "listing_view",
+          },
+        });
+
+        expect(() => {
+          subject.validate();
+        }).not.toThrow();
+      });
+      it("throws an error if preset is not set in all collectionSpecificSearchParameters", () => {
+        const subject = new Configuration({
+          collectionSpecificSearchParameters: {
+            collection1: {
+              preset: "listing_view_1",
+            },
+            collection2: {},
+          },
+        });
+
+        expect(() => {
+          subject.validate();
+        }).toThrow(/Missing parameter/);
+      });
+      it("does not throw an error if preset or query_by is set in all collectionSpecificSearchParameters", () => {
+        const subject = new Configuration({
+          collectionSpecificSearchParameters: {
+            collection1: {
+              preset: "listing_view_1",
+            },
+            collection2: {
+              query_by: "name",
+            },
+          },
+        });
+
+        expect(() => {
+          subject.validate();
+        }).not.toThrow();
+      });
+      it("does not throw an error if preset is set in all collectionSpecificSearchParameters", () => {
+        const subject = new Configuration({
+          collectionSpecificSearchParameters: {
+            collection1: {
+              preset: "listing_view_1",
+            },
+            collection2: {
+              preset: "listing_view_2",
             },
           },
         });
