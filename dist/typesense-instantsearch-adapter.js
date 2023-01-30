56abed15bc7db0aa040bfca1fa3b404ff0ca0f3f
Jason Bosco <mail@jasonbos.co>
2023-01-30 10:32:35 -0600

    Build

--- dist/typesense-instantsearch-adapter.js
+++ dist/typesense-instantsearch-adapter.js
@@ -4792,6 +4792,8 @@ var Metrics_1 = __importDefault(__webpack_require__(/*! ./Metrics */ "./node_mod
 var Health_1 = __importDefault(__webpack_require__(/*! ./Health */ "./node_modules/typesense/lib/Typesense/Health.js"));
 var Operations_1 = __importDefault(__webpack_require__(/*! ./Operations */ "./node_modules/typesense/lib/Typesense/Operations.js"));
 var MultiSearch_1 = __importDefault(__webpack_require__(/*! ./MultiSearch */ "./node_modules/typesense/lib/Typesense/MultiSearch.js"));
+var Presets_1 = __importDefault(__webpack_require__(/*! ./Presets */ "./node_modules/typesense/lib/Typesense/Presets.js"));
+var Preset_1 = __importDefault(__webpack_require__(/*! ./Preset */ "./node_modules/typesense/lib/Typesense/Preset.js"));
 var Client = /** @class */ (function () {
     function Client(options) {
         var _a;
@@ -4809,6 +4811,8 @@ var Client = /** @class */ (function () {
         this.individualAliases = {};
         this._keys = new Keys_1.default(this.apiCall);
         this.individualKeys = {};
+        this._presets = new Presets_1.default(this.apiCall);
+        this.individualPresets = {};
     }
     Client.prototype.collections = function (collectionName) {
         if (collectionName === undefined) {
@@ -4841,6 +4845,17 @@ var Client = /** @class */ (function () {
                 this.individualKeys[id] = new Key_1.default(id, this.apiCall);
             }
             return this.individualKeys[id];
+        }
+    };
+    Client.prototype.presets = function (id) {
+        if (id === undefined) {
+            return this._presets;
+        }
+        else {
+            if (this.individualPresets[id] === undefined) {
+                this.individualPresets[id] = new Preset_1.default(id, this.apiCall);
+            }
+            return this.individualPresets[id];
         }
     };
     return Client;
@@ -6629,6 +6644,165 @@ var Overrides = /** @class */ (function () {
 }());
 exports["default"] = Overrides;
 //# sourceMappingURL=Overrides.js.map
+
+/***/ }),
+
+/***/ "./node_modules/typesense/lib/Typesense/Preset.js":
+/*!********************************************************!*\
+  !*** ./node_modules/typesense/lib/Typesense/Preset.js ***!
+  \********************************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+var Presets_1 = __importDefault(__webpack_require__(/*! ./Presets */ "./node_modules/typesense/lib/Typesense/Presets.js"));
+var Preset = /** @class */ (function () {
+    function Preset(presetId, apiCall) {
+        this.presetId = presetId;
+        this.apiCall = apiCall;
+    }
+    Preset.prototype.retrieve = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                return [2 /*return*/, this.apiCall.get(this.endpointPath())];
+            });
+        });
+    };
+    Preset.prototype.delete = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                return [2 /*return*/, this.apiCall.delete(this.endpointPath())];
+            });
+        });
+    };
+    Preset.prototype.endpointPath = function () {
+        return "".concat(Presets_1.default.RESOURCEPATH, "/").concat(this.presetId);
+    };
+    return Preset;
+}());
+exports["default"] = Preset;
+//# sourceMappingURL=Preset.js.map
+
+/***/ }),
+
+/***/ "./node_modules/typesense/lib/Typesense/Presets.js":
+/*!*********************************************************!*\
+  !*** ./node_modules/typesense/lib/Typesense/Presets.js ***!
+  \*********************************************************/
+/***/ (function(__unused_webpack_module, exports) {
+
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+var RESOURCEPATH = '/presets';
+var Presets = /** @class */ (function () {
+    function Presets(apiCall) {
+        this.apiCall = apiCall;
+    }
+    Presets.prototype.upsert = function (presetId, params) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                return [2 /*return*/, this.apiCall.put(this.endpointPath(presetId), params)];
+            });
+        });
+    };
+    Presets.prototype.retrieve = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                return [2 /*return*/, this.apiCall.get(this.endpointPath())];
+            });
+        });
+    };
+    Presets.prototype.endpointPath = function (operation) {
+        return "".concat(Presets.RESOURCEPATH).concat(operation === undefined ? '' : '/' + operation);
+    };
+    Object.defineProperty(Presets, "RESOURCEPATH", {
+        get: function () {
+            return RESOURCEPATH;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Presets;
+}());
+exports["default"] = Presets;
+//# sourceMappingURL=Presets.js.map
 
 /***/ }),
 
