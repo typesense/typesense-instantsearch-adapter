eebade9bb36b8ac80915801d1f86ac7c6609e601
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-06-13 12:30:56 +0300

    test: add union search demo and integration tests
    - Add `union_search.html` demo page for testing union functionality
    - Create `union_search.js` with multi-collection search configuration
    - Add end-to-end tests for union search behavior
    - Update build scripts to include union search page
    - Verify merged results from multiple collections

---
+++ test/support/testground/src/union_search.js
@@ -0,0 +1,83 @@
+import instantsearch from "instantsearch.js";
+import { searchBox, pagination, hits, stats, index } from "instantsearch.js/es/widgets";
+
+import TypesenseInstantSearchAdapter from "typesense-instantsearch-adapter";
+
+const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
+  server: {
+    apiKey: "xyz", // Be sure to use an API key that only has search permissions, since this is exposed in the browser
+    nodes: [
+      {
+        host: "localhost",
+        port: "8108",
+        protocol: "http",
+      },
+    ],
+  },
+  // Enable union search to merge results from multiple collections
+  union: true,
+  // The following parameters are directly passed to Typesense's search API endpoint.
+  //  So you can pass any parameters supported by the search endpoint below.
+  // Parameters set in collectionSpecificSearchParameters override parameters set in additionalSearchParameters
+  collectionSpecificSearchParameters: {
+    products: {
+      query_by: "name,description,categories",
+    },
+    brands: {
+      query_by: "name",
+    },
+  },
+});
+
+const searchClient = typesenseInstantsearchAdapter.searchClient;
+const search = instantsearch({
+  searchClient,
+  indexName: "products",
+  routing: true,
+});
+
+// ============ Begin Widget Configuration
+search.addWidgets([
+  searchBox({
+    container: "#searchbox",
+  }),
+  pagination({
+    container: "#pagination",
+  }),
+  index({ indexName: "products" }).addWidgets([
+    hits({
+      container: "#hits",
+      templates: {
+        item: `
+        <div>
+          <img src="{{image}}" align="left" alt="{{name}}" />
+          <div class="hit-name">
+            {{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
+          </div>
+          <div class="hit-description">
+            {{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}
+          </div>
+          <div class="hit-price">\${{price}}</div>
+          <div class="hit-rating">Categories: {{categories}}</div>
+          <div class="hit-rating">Rating: {{rating}}</div>
+          <div class="hit-free-shipping">Free Shipping: {{free_shipping}}</div>
+        </div>
+      `,
+      },
+    }),
+    stats({
+      container: "#stats",
+      templates: {
+        text: `
+      {{#hasNoResults}}No results{{/hasNoResults}}
+      {{#hasOneResult}}1 result{{/hasOneResult}}
+      {{#hasManyResults}}{{#helpers.formatNumber}}{{nbHits}}{{/helpers.formatNumber}} results{{/hasManyResults}}
+      found in {{processingTimeMS}}ms for {{query}}
+    `,
+      },
+    }),
+  ]),
+  index({ indexName: "brands" }),
+]);
+
+search.start();
